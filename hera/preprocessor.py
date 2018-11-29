a735d1b8c47cea6a47aa959453bd15e6aa7ec830
Ian Fisher <iafisher@protonmail.com>
2018-11-28 21:22:17 -0500

    Simplify preprocessor further

--- hera/preprocessor.py
+++ hera/preprocessor.py
@@ -37,13 +37,15 @@ class Preprocessor:
     def preprocess(self, program):
         """See docstring for module-level preprocess for details."""
         for op in program:
-            try:
-                verifier = getattr(self, "verify_" + op.name.lower())
-            except AttributeError:
-                pass
-            else:
+            params = _params_map.get(op.name.upper())
+            if params is None and op.name.upper().startswith("B"):
+                if op.name.upper().endswith("R") and len(op.name) > 2:
+                    params = (I8,)
+                else:
+                    params = (REGISTER_OR_LABEL,)
+            if params is not None:
                 try:
-                    verifier(*op.args)
+                    verify_args(op.name, params, op.args)
                 except HERAError as e:
                     # Fill in line number if not already present.
                     if not e.line:
@@ -132,250 +134,6 @@ class Preprocessor:
             else:
                 pc += 1
 
-    # Constants to pass to verify_base
-    REGISTER = "r"
-    REGISTER_OR_LABEL = "rl"
-    STRING = "s"
-    U4 = range(0, 2 ** 4)
-    U5 = range(0, 2 ** 5)
-    U16 = range(0, 2 ** 16)
-    I8 = range(-128, 256)
-
-    def assert_args(self, name, expected, got):
-        """Assert that the given args match the expected ones and raise a
-        HERAError otherwise. `name` is the name of the HERA op. `expected` is
-        a tuple or list of constants (REGISTER, U16, etc., defined above)
-        representing the expected argument types to the operation. `args` is
-        a tuple or list of the actual arguments given.
-        """
-        if len(got) < len(expected):
-            raise HERAError(
-                "too few args to {} (expected {})".format(name, len(expected))
-            )
-
-        if len(expected) < len(got):
-            raise HERAError(
-                "too many args to {} (expected {})".format(name, len(expected))
-            )
-
-        ordinals = ["first", "second", "third"]
-        for ordinal, pattern, arg in zip(ordinals, expected, got):
-            prefix = "{} arg to {} ".format(ordinal, name)
-            error = self.assert_one_arg(pattern, arg)
-            if error:
-                raise HERAError(prefix + error, line=arg.line, column=arg.column)
-
-    def assert_one_arg(self, pattern, arg):
-        """Assert that the argument matches the pattern. Return a string stating the
-        error if it doesn't, return None otherwise.
-        """
-        if pattern == self.REGISTER:
-            if not isinstance(arg, Token) or arg.type != "REGISTER":
-                return "not a register"
-
-            try:
-                register_to_index(arg)
-            except ValueError:
-                return "not a valid register"
-        elif pattern == self.REGISTER_OR_LABEL:
-            if not isinstance(arg, Token):
-                return "not a register or label"
-
-            if arg.type == "REGISTER":
-                try:
-                    register_to_index(arg)
-                except ValueError:
-                    return "not a valid register"
-            elif arg.type != "SYMBOL":
-                return "not a register or label"
-        elif isinstance(pattern, range):
-            if is_symbol(arg):
-                # Symbols will be resolved later.
-                return None
-
-            if not isinstance(arg, int):
-                return "not an integer"
-            if arg not in pattern:
-                if pattern.start == 0 and arg < 0:
-                    return "must not be negative"
-                else:
-                    return "out of range"
-        else:
-            raise RuntimeError(
-                "unknown pattern in Preprocessor.assert_one_arg", pattern
-            )
-
-    def verify_set(self, *args):
-        self.assert_args("SET", [self.REGISTER, range(-32768, 65536)], args)
-
-    def verify_setlo(self, *args):
-        self.assert_args("SETLO", [self.REGISTER, range(-128, 256)], args)
-
-    def verify_sethi(self, *args):
-        self.assert_args("SETHI", [self.REGISTER, range(0, 256)], args)
-
-    def verify_and(self, *args):
-        self.assert_args("AND", [self.REGISTER] * 3, args)
-
-    def verify_or(self, *args):
-        self.assert_args("OR", [self.REGISTER] * 3, args)
-
-    def verify_add(self, *args):
-        self.assert_args("ADD", [self.REGISTER] * 3, args)
-
-    def verify_sub(self, *args):
-        self.assert_args("SUB", [self.REGISTER] * 3, args)
-
-    def verify_mul(self, *args):
-        self.assert_args("MUL", [self.REGISTER] * 3, args)
-
-    def verify_xor(self, *args):
-        self.assert_args("XOR", [self.REGISTER] * 3, args)
-
-    def verify_inc(self, *args):
-        self.assert_args("INC", [self.REGISTER, range(1, 65)], args)
-
-    def verify_dec(self, *args):
-        self.assert_args("DEC", [self.REGISTER, range(1, 65)], args)
-
-    def verify_lsl(self, *args):
-        self.assert_args("LSL", [self.REGISTER] * 2, args)
-
-    def verify_lsr(self, *args):
-        self.assert_args("LSR", [self.REGISTER] * 2, args)
-
-    def verify_lsl8(self, *args):
-        self.assert_args("LSL8", [self.REGISTER] * 2, args)
-
-    def verify_lsr8(self, *args):
-        self.assert_args("LSR8", [self.REGISTER] * 2, args)
-
-    def verify_asl(self, *args):
-        self.assert_args("ASL", [self.REGISTER] * 2, args)
-
-    def verify_asr(self, *args):
-        self.assert_args("ASR", [self.REGISTER] * 2, args)
-
-    def verify_savef(self, *args):
-        self.assert_args("SAVEF", [self.REGISTER], args)
-
-    def verify_rstrf(self, *args):
-        self.assert_args("RSTRF", [self.REGISTER], args)
-
-    def verify_fon(self, *args):
-        self.assert_args("FON", [self.U4], args)
-
-    def verify_foff(self, *args):
-        self.assert_args("FOFF", [self.U4], args)
-
-    def verify_fset5(self, *args):
-        self.assert_args("FSET5", [self.U4], args)
-
-    def verify_fset4(self, *args):
-        self.assert_args("FSET4", [self.U4], args)
-
-    def verify_load(self, *args):
-        self.assert_args("LOAD", [self.REGISTER, self.U5, self.REGISTER], args)
-
-    def verify_store(self, *args):
-        self.assert_args("STORE", [self.REGISTER, self.U5, self.REGISTER], args)
-
-    def verify_br(self, *args):
-        self.assert_args("BR", [self.REGISTER_OR_LABEL], args)
-
-    def verify_brr(self, *args):
-        self.assert_args("BRR", [self.I8], args)
-
-    def verify_bl(self, *args):
-        self.assert_args("BL", [self.REGISTER_OR_LABEL], args)
-
-    def verify_blr(self, *args):
-        self.assert_args("BLR", [self.I8], args)
-
-    def verify_bge(self, *args):
-        self.assert_args("BGE", [self.REGISTER_OR_LABEL], args)
-
-    def verify_bger(self, *args):
-        self.assert_args("BGER", [self.I8], args)
-
-    def verify_ble(self, *args):
-        self.assert_args("BLE", [self.REGISTER_OR_LABEL], args)
-
-    def verify_bler(self, *args):
-        self.assert_args("BLER", [self.I8], args)
-
-    def verify_bg(self, *args):
-        self.assert_args("BG", [self.REGISTER_OR_LABEL], args)
-
-    def verify_bgr(self, *args):
-        self.assert_args("BGR", [self.I8], args)
-
-    def verify_bule(self, *args):
-        self.assert_args("BULE", [self.REGISTER_OR_LABEL], args)
-
-    def verify_buler(self, *args):
-        self.assert_args("BULER", [self.I8], args)
-
-    def verify_bug(self, *args):
-        self.assert_args("BUG", [self.REGISTER_OR_LABEL], args)
-
-    def verify_bugr(self, *args):
-        self.assert_args("BUGR", [self.I8], args)
-
-    def verify_bz(self, *args):
-        self.assert_args("BZ", [self.REGISTER_OR_LABEL], args)
-
-    def verify_bzr(self, *args):
-        self.assert_args("BZR", [self.I8], args)
-
-    def verify_bnz(self, *args):
-        self.assert_args("BNZ", [self.REGISTER_OR_LABEL], args)
-
-    def verify_bnzr(self, *args):
-        self.assert_args("BNZR", [self.I8], args)
-
-    def verify_bc(self, *args):
-        self.assert_args("BC", [self.REGISTER_OR_LABEL], args)
-
-    def verify_bcr(self, *args):
-        self.assert_args("BCR", [self.I8], args)
-
-    def verify_bnc(self, *args):
-        self.assert_args("BNC", [self.REGISTER_OR_LABEL], args)
-
-    def verify_bncr(self, *args):
-        self.assert_args("BNCR", [self.I8], args)
-
-    def verify_bs(self, *args):
-        self.assert_args("BS", [self.REGISTER_OR_LABEL], args)
-
-    def verify_bsr(self, *args):
-        self.assert_args("BSR", [self.I8], args)
-
-    def verify_bns(self, *args):
-        self.assert_args("BNS", [self.REGISTER_OR_LABEL], args)
-
-    def verify_bnsr(self, *args):
-        self.assert_args("BNSR", [self.I8], args)
-
-    def verify_bv(self, *args):
-        self.assert_args("BV", [self.REGISTER_OR_LABEL], args)
-
-    def verify_bvr(self, *args):
-        self.assert_args("BVR", [self.I8], args)
-
-    def verify_bnv(self, *args):
-        self.assert_args("BNV", [self.REGISTER_OR_LABEL], args)
-
-    def verify_bnvr(self, *args):
-        self.assert_args("BNVR", [self.I8], args)
-
-    def verify_call(self, *args):
-        self.assert_args("CALL", [self.REGISTER, self.REGISTER_OR_LABEL], args)
-
-    def verify_return(self, *args):
-        self.assert_args("RETURN", [self.REGISTER, self.REGISTER_OR_LABEL], args)
-
     def convert_set(self, d, v):
         if isinstance(v, int):
             v = to_u16(v)
@@ -438,6 +196,109 @@ class Preprocessor:
             Op("SETHI", ["R11", 0xFF]),
             Op("XOR", [d, "R11", b]),
         ]
+
+
+# Constants to pass to verify_base
+REGISTER = "r"
+REGISTER_OR_LABEL = "rl"
+STRING = "s"
+U4 = range(0, 2 ** 4)
+U5 = range(0, 2 ** 5)
+U16 = range(0, 2 ** 16)
+I8 = range(-2 ** 7, 2 ** 8)
+I16 = range(-2 ** 15, 2 ** 16)
+
+
+_params_map = {
+    "SET": (REGISTER, I16),
+    "SETLO": (REGISTER, I8),
+    "SETHI": (REGISTER, I8),
+    "AND": (REGISTER, REGISTER, REGISTER),
+    "OR": (REGISTER, REGISTER, REGISTER),
+    "ADD": (REGISTER, REGISTER, REGISTER),
+    "SUB": (REGISTER, REGISTER, REGISTER),
+    "MUL": (REGISTER, REGISTER, REGISTER),
+    "XOR": (REGISTER, REGISTER, REGISTER),
+    "INC": (REGISTER, range(1, 65)),
+    "DEC": (REGISTER, range(1, 65)),
+    "LSL": (REGISTER, REGISTER),
+    "LSR": (REGISTER, REGISTER),
+    "LSL8": (REGISTER, REGISTER),
+    "LSR8": (REGISTER, REGISTER),
+    "ASL": (REGISTER, REGISTER),
+    "ASR": (REGISTER, REGISTER),
+    "SAVEF": (REGISTER,),
+    "RSTRF": (REGISTER,),
+    "FON": (U4,),
+    "FOFF": (U4,),
+    "FSET5": (U4,),
+    "FSET4": (U4,),
+    "LOAD": (REGISTER, U5, REGISTER),
+    "STORE": (REGISTER, U5, REGISTER),
+    "CALL": (REGISTER, REGISTER_OR_LABEL),
+    "RETURN": (REGISTER, REGISTER_OR_LABEL),
+}
+
+
+def verify_args(name, expected, got):
+    """Verify that the given args match the expected ones and raise a HERAError 
+    otherwise. `name` is the name of the HERA op. `expected` is a tuple or list of 
+    constants (REGISTER, U16, etc., defined above) representing the expected argument 
+    types to the operation. `args` is a tuple or list of the actual arguments given.
+    """
+    if len(got) < len(expected):
+        raise HERAError("too few args to {} (expected {})".format(name, len(expected)))
+
+    if len(expected) < len(got):
+        raise HERAError("too many args to {} (expected {})".format(name, len(expected)))
+
+    ordinals = ["first", "second", "third"]
+    for ordinal, pattern, arg in zip(ordinals, expected, got):
+        prefix = "{} arg to {} ".format(ordinal, name)
+        error = verify_one_arg(pattern, arg)
+        if error:
+            raise HERAError(prefix + error, line=arg.line, column=arg.column)
+
+
+def verify_one_arg(pattern, arg):
+    """Verify that the argument matches the pattern. Return a string stating the error
+    if it doesn't, return None otherwise.
+    """
+    if pattern == REGISTER:
+        if not isinstance(arg, Token) or arg.type != "REGISTER":
+            return "not a register"
+
+        try:
+            register_to_index(arg)
+        except ValueError:
+            return "not a valid register"
+    elif pattern == REGISTER_OR_LABEL:
+        if not isinstance(arg, Token):
+            return "not a register or label"
+
+        if arg.type == "REGISTER":
+            try:
+                register_to_index(arg)
+            except ValueError:
+                return "not a valid register"
+        elif arg.type != "SYMBOL":
+            return "not a register or label"
+    elif isinstance(pattern, range):
+        if is_symbol(arg):
+            # Symbols will be resolved later.
+            return None
+
+        if not isinstance(arg, int):
+            return "not an integer"
+        if arg not in pattern:
+            if pattern.start == 0 and arg < 0:
+                return "must not be negative"
+            else:
+                return "out of range"
+    else:
+        raise RuntimeError(
+            "unknown pattern in hera.preprocessor.verify_one_arg", pattern
+        )
 
 
 def copy_token(val, otkn):
--- test/test_preprocessor.py
+++ test/test_preprocessor.py
@@ -3,7 +3,16 @@ import pytest
 from lark import Token
 
 from hera.parser import Op
-from hera.preprocessor import preprocess, Preprocessor, HERA_DATA_START
+from hera.preprocessor import (
+    preprocess,
+    verify_args,
+    Preprocessor,
+    HERA_DATA_START,
+    REGISTER,
+    REGISTER_OR_LABEL,
+    U4,
+    U16,
+)
 from hera.utils import HERAError, IntToken
 
 
@@ -204,357 +213,71 @@ def test_preprocess_constant(ppr):
     assert preprocess(program) == [Op("SETLO", ["R1", 100]), Op("SETHI", ["R1", 0])]
 
 
-def test_assert_args_with_too_few(ppr):
+def test_verify_args_with_too_few():
     with pytest.raises(HERAError) as e:
-        ppr.assert_args("", [ppr.REGISTER, ppr.REGISTER], [REG("R1")])
+        verify_args("", [REGISTER, REGISTER], [REG("R1")])
     assert "too few" in str(e)
 
 
-def test_assert_args_with_too_many(ppr):
+def test_verify_args_with_too_many():
     with pytest.raises(HERAError) as e:
-        ppr.assert_args("", [ppr.REGISTER], [REG("R1"), IntToken(10)])
+        verify_args("", [REGISTER], [REG("R1"), IntToken(10)])
     assert "too many" in str(e)
 
 
-def test_assert_args_with_wrong_type(ppr):
+def test_verify_args_with_wrong_type():
     with pytest.raises(HERAError) as e1:
-        ppr.assert_args("", [ppr.REGISTER], [IntToken(10)])
+        verify_args("", [REGISTER], [IntToken(10)])
     assert "not a register" in str(e1)
 
     with pytest.raises(HERAError) as e2:
-        ppr.assert_args("", [ppr.U16], [REG("R1")])
+        verify_args("", [U16], [REG("R1")])
     assert "not an integer" in str(e2)
 
 
-def test_assert_args_with_u16_out_of_range(ppr):
+def test_verify_args_with_u16_out_of_range():
     with pytest.raises(HERAError) as e:
-        ppr.assert_args("", [ppr.U16], [IntToken(65536)])
+        verify_args("", [U16], [IntToken(65536)])
     assert "out of range" in str(e)
 
 
-def test_assert_args_with_negative_u16(ppr):
+def test_verify_args_with_negative_u16():
     with pytest.raises(HERAError) as e:
-        ppr.assert_args("", [ppr.U16], [IntToken(-1)])
+        verify_args("", [U16], [IntToken(-1)])
     assert "must not be negative" in str(e)
 
 
-def test_assert_args_with_u4_out_of_range(ppr):
+def test_verify_args_with_u4_out_of_range():
     with pytest.raises(HERAError) as e1:
-        ppr.assert_args("", [ppr.U4], [IntToken(16)])
+        verify_args("", [U4], [IntToken(16)])
     assert "out of range" in str(e1)
 
     with pytest.raises(HERAError) as e2:
-        ppr.assert_args("", [ppr.U4], [IntToken(-1)])
+        verify_args("", [U4], [IntToken(-1)])
     assert "must not be negative" in str(e2)
 
 
-def test_assert_args_with_range_object(ppr):
+def test_verify_args_with_range_object():
     with pytest.raises(HERAError) as e1:
-        ppr.assert_args("", [range(-10, 10)], [IntToken(-11)])
+        verify_args("", [range(-10, 10)], [IntToken(-11)])
     assert "out of range" in str(e1)
 
     with pytest.raises(HERAError) as e2:
-        ppr.assert_args("", [range(-10, 10)], [IntToken(10)])
+        verify_args("", [range(-10, 10)], [IntToken(10)])
     assert "out of range" in str(e2)
 
     with pytest.raises(HERAError) as e3:
-        ppr.assert_args("", [range(-10, 10)], [REG("R1")])
+        verify_args("", [range(-10, 10)], [REG("R1")])
     assert "not an integer" in str(e3)
 
     r = range(-10, 10)
-    ppr.assert_args("", [r, r, r], [5, -10, 9])
+    verify_args("", [r, r, r], [5, -10, 9])
 
 
-def test_assert_args_with_constant_symbol(ppr):
-    ppr.assert_args("", [range(0, 100)], [Token("SYMBOL", "n")])
+def test_verify_args_with_constant_symbol():
+    verify_args("", [range(0, 100)], [Token("SYMBOL", "n")])
 
 
-def test_assert_args_with_register_or_label(ppr):
-    ppr.assert_args("", [ppr.REGISTER_OR_LABEL], [Token("SYMBOL", "n")])
-    ppr.assert_args("", [ppr.REGISTER_OR_LABEL], [REG("R1")])
-
-
-def test_verify_set_good(ppr):
-    ppr.verify_set(REG("R1"), IntToken(-5))
-
-
-def test_verify_set_bad(ppr):
-    with pytest.raises(HERAError) as e:
-        ppr.verify_set(IntToken(10), REG("R1"))
-    assert "SET" in str(e)
-    assert "not a register" in str(e)
-
-
-def test_verify_setlo_good(ppr):
-    ppr.verify_setlo(REG("R1"), IntToken(-5))
-
-
-def test_verify_setlo_bad(ppr):
-    with pytest.raises(HERAError) as e:
-        ppr.verify_setlo(REG("R1"), REG("R2"))
-    assert "SETLO" in str(e)
-    assert "not an integer" in str(e)
-
-
-def test_verify_sethi_good(ppr):
-    ppr.verify_sethi(REG("R1"), IntToken(5))
-
-
-def test_verify_sethi_bad(ppr):
-    with pytest.raises(HERAError) as e:
-        ppr.verify_sethi(REG("R1"), REG("R2"))
-    assert "SETHI" in str(e)
-    assert "not an integer" in str(e)
-
-
-def test_verify_and_good(ppr):
-    ppr.verify_and(REG("R1"), REG("R2"), REG("R3"))
-
-
-def test_verify_and_bad(ppr):
-    with pytest.raises(HERAError) as e:
-        ppr.verify_and(REG("R1"), REG("R2"))
-    assert "AND" in str(e)
-    assert "too few args" in str(e)
-
-
-def test_verify_or_good(ppr):
-    ppr.verify_or(REG("R1"), REG("R2"), REG("R3"))
-
-
-def test_verify_or_bad(ppr):
-    with pytest.raises(HERAError) as e:
-        ppr.verify_or(REG("R1"), REG("R2"), REG("R3"), REG("R4"))
-    assert "OR" in str(e)
-    assert "too many args" in str(e)
-
-
-def test_verify_add_good(ppr):
-    ppr.verify_add(REG("R1"), REG("R2"), REG("R3"))
-
-
-def test_verify_add_bad(ppr):
-    with pytest.raises(HERAError) as e:
-        ppr.verify_add(REG("R1"), REG("R2"), IntToken(17))
-    assert "ADD" in str(e)
-    assert "not a register" in str(e)
-
-
-def test_verify_sub_good(ppr):
-    ppr.verify_sub(REG("R1"), REG("R2"), REG("R3"))
-
-
-def test_verify_sub_bad(ppr):
-    with pytest.raises(HERAError) as e:
-        ppr.verify_sub(REG("R1"), REG("R2"), IntToken(17))
-    assert "SUB" in str(e)
-    assert "not a register" in str(e)
-
-
-def test_verify_mul_good(ppr):
-    ppr.verify_mul(REG("R1"), REG("R2"), REG("R3"))
-
-
-def test_verify_mul_bad(ppr):
-    with pytest.raises(HERAError) as e:
-        ppr.verify_mul(REG("R1"), IntToken(17), REG("R2"))
-    assert "MUL" in str(e)
-    assert "not a register" in str(e)
-
-
-def test_verify_xor_good(ppr):
-    ppr.verify_xor(REG("R1"), REG("R2"), REG("R3"))
-
-
-def test_verify_xor_bad(ppr):
-    with pytest.raises(HERAError) as e:
-        ppr.verify_xor(REG("R1"), IntToken(17), REG("R2"))
-    assert "XOR" in str(e)
-    assert "not a register" in str(e)
-
-
-def test_verify_inc_good(ppr):
-    ppr.verify_inc(REG("R1"), IntToken(64))
-
-
-def test_verify_inc_bad(ppr):
-    with pytest.raises(HERAError) as e:
-        ppr.verify_inc(REG("R1"), IntToken(65))
-    assert "INC" in str(e)
-    assert "out of range" in str(e)
-
-
-def test_verify_dec_good(ppr):
-    ppr.verify_dec(REG("R1"), IntToken(64))
-
-
-def test_verify_dec_bad(ppr):
-    with pytest.raises(HERAError) as e:
-        ppr.verify_dec(REG("R1"), IntToken(-1))
-    assert "DEC" in str(e)
-    assert "out of range" in str(e)
-
-
-def test_verify_lsl_good(ppr):
-    ppr.verify_lsl(REG("R8"), REG("R7"))
-
-
-def test_verify_lsl_bad(ppr):
-    with pytest.raises(HERAError) as e:
-        ppr.verify_lsl(IntToken(10), REG("R6"))
-    assert "LSL" in str(e)
-    assert "not a register" in str(e)
-
-
-def test_verify_lsr_good(ppr):
-    ppr.verify_lsr(REG("R8"), REG("R7"))
-
-
-def test_verify_lsr_bad(ppr):
-    with pytest.raises(HERAError) as e:
-        ppr.verify_lsr(REG("R6"), IntToken(10))
-    assert "LSR" in str(e)
-    assert "not a register" in str(e)
-
-
-def test_verify_lsl8_good(ppr):
-    ppr.verify_lsl8(REG("R8"), REG("R7"))
-
-
-def test_verify_lsl8_bad(ppr):
-    with pytest.raises(HERAError) as e:
-        ppr.verify_lsl8(REG("R6"), IntToken(10))
-    assert "LSL8" in str(e)
-    assert "not a register" in str(e)
-
-
-def test_verify_lsr8_good(ppr):
-    ppr.verify_lsr8(REG("R8"), REG("R7"))
-
-
-def test_verify_lsr8_bad(ppr):
-    with pytest.raises(HERAError) as e:
-        ppr.verify_lsr8(REG("R6"), IntToken(10))
-    assert "LSR8" in str(e)
-    assert "not a register" in str(e)
-
-
-def test_verify_asl_good(ppr):
-    ppr.verify_asl(REG("R8"), REG("R7"))
-
-
-def test_verify_asl_bad(ppr):
-    with pytest.raises(HERAError) as e:
-        ppr.verify_asl(REG("R6"), IntToken(10))
-    assert "ASL" in str(e)
-    assert "not a register" in str(e)
-
-
-def test_verify_asr_good(ppr):
-    ppr.verify_asr(REG("R8"), REG("R7"))
-
-
-def test_verify_asr_bad(ppr):
-    with pytest.raises(HERAError) as e:
-        ppr.verify_asr(REG("R6"), IntToken(10))
-    assert "ASR" in str(e)
-    assert "not a register" in str(e)
-
-
-def test_verify_savef_good(ppr):
-    ppr.verify_savef(REG("R1"))
-
-
-def test_verify_savef_bad(ppr):
-    with pytest.raises(HERAError) as e:
-        ppr.verify_savef(REG("R1"), REG("R2"))
-    assert "SAVEF" in str(e)
-    assert "too many" in str(e)
-
-
-def test_verify_rstrf_good(ppr):
-    ppr.verify_rstrf(REG("R1"))
-
-
-def test_verify_rstrf_bad(ppr):
-    with pytest.raises(HERAError) as e:
-        ppr.verify_rstrf(REG("R1"), REG("R2"))
-    assert "RSTRF" in str(e)
-    assert "too many" in str(e)
-
-
-def test_verify_fon_good(ppr):
-    ppr.verify_fon(IntToken(4))
-
-
-def test_verify_fon_bad(ppr):
-    with pytest.raises(HERAError) as e:
-        ppr.verify_fon(IntToken(16))
-    assert "FON" in str(e)
-    assert "out of range" in str(e)
-
-
-def test_verify_foff_good(ppr):
-    ppr.verify_foff(IntToken(4))
-
-
-def test_verify_foff_bad(ppr):
-    with pytest.raises(HERAError) as e:
-        ppr.verify_foff(IntToken(16))
-    assert "FOFF" in str(e)
-    assert "out of range" in str(e)
-
-
-def test_verify_fset5_good(ppr):
-    ppr.verify_fset5(IntToken(4))
-
-
-def test_verify_fset5_bad(ppr):
-    with pytest.raises(HERAError) as e:
-        ppr.verify_fset5(IntToken(16))
-    assert "FSET5" in str(e)
-    assert "out of range" in str(e)
-
-
-def test_verify_fset4_good(ppr):
-    ppr.verify_fset4(IntToken(4))
-
-
-def test_verify_fset4_bad(ppr):
-    with pytest.raises(HERAError) as e:
-        ppr.verify_fset4(IntToken(16))
-    assert "FSET4" in str(e)
-    assert "out of range" in str(e)
-
-
-def test_verify_load_good(ppr):
-    ppr.verify_load(REG("R1"), IntToken(0), REG("R2"))
-
-
-def test_verify_load_bad(ppr):
-    with pytest.raises(HERAError) as e:
-        ppr.verify_load(REG("R1"), REG("R2"))
-    assert "LOAD" in str(e)
-    assert "too few" in str(e)
-
-
-def test_verify_store_good(ppr):
-    ppr.verify_store(REG("R1"), IntToken(0), REG("R2"))
-
-
-def test_verify_store_bad(ppr):
-    with pytest.raises(HERAError) as e:
-        ppr.verify_load(REG("R1"), IntToken(32), REG("R2"))
-    assert "LOAD" in str(e)
-    assert "out of range" in str(e)
-
-
-def test_verify_br_good(ppr):
-    ppr.verify_br(REG("R1"))
-
-
-def test_verify_br_bad(ppr):
-    with pytest.raises(HERAError) as e:
-        ppr.verify_br(REG("R1"), REG("R2"))
-    assert "BR" in str(e)
-    assert "too many" in str(e)
+def test_verify_args_with_register_or_label():
+    verify_args("", [REGISTER_OR_LABEL], [Token("SYMBOL", "n")])
+    verify_args("", [REGISTER_OR_LABEL], [REG("R1")])
