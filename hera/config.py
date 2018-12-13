25a0201d8b12efc6acc51dd90d60f3b884853bb8
Ian Fisher <iafisher@protonmail.com>
2018-12-13 15:24:51 -0500

    Document hera/config.py

--- hera/config.py
+++ hera/config.py
@@ -1,15 +1,16 @@
+"""Global configuration set by the main function."""
+
+# The lines of the program being processed (a list of strings). Used for error messages.
 LINES = None
-
-
-# ANSI color codes (https://stackoverflow.com/questions/4842424/)
-# When the --no-color flag is specified, these constants are set to the empty
-# string, so they can be used unconditionally in your code but will still obey
-# the flag value.
 
 
 def _make_ansi(*params):
     return "\033[" + ";".join(map(str, params)) + "m"
 
+
+# ANSI color codes (https://stackoverflow.com/questions/4842424/)
+# When the --no-color flag is specified, these constants are set to the empty string, so
+# they can be used unconditionally in your code but will still obey the flag value.
 
 ANSI_RED_BOLD = _make_ansi(31, 1)
 ANSI_MAGENTA_BOLD = _make_ansi(35, 1)
