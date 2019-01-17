ed5142abb8f91cd1d586fe80480cad9bc7e7ba5b
Ian Fisher <iafisher@protonmail.com>
2019-01-17 09:31:28 -0500

    Move debugging mini-language to its own module

---
+++ hera/minilanguage.py
@@ -0,0 +1,184 @@
+"""The expression mini-language for the debugger.
+
+Author:  Ian Fisher (iafisher@protonmail.com)
+Version: January 2019
+"""
+from collections import namedtuple
+
+
+def parse(line):
+    """Return a parse tree for the line of code. Raise a SyntaxError if it is not
+    well-formatted.
+    """
+    return MiniParser(MiniLexer(line)).parse()
+
+
+class MiniParser:
+    """A parser for the debugger's expression mini-language.
+
+      start := expr | assign
+
+      expr := mem | REGISTER | INT
+      mem  := MEM LBRACKET expr RBRACKET
+
+      assign := lvalue ASSIGN expr
+      lvalue := mem | REGISTER
+    """
+
+    def __init__(self, lexer):
+        self.lexer = lexer
+
+    def parse(self):
+        tree = self.match_expr()
+        tkn = self.lexer.next_token()
+        if tkn[0] == TOKEN_EOF:
+            return tree
+        elif tkn[0] == TOKEN_ASSIGN:
+            if isinstance(tree, IntNode):
+                raise SyntaxError("integer cannot be assigned to")
+
+            rhs = self.match_expr()
+            if self.lexer.next_token()[0] == TOKEN_EOF:
+                return AssignNode(tree, rhs)
+            else:
+                raise SyntaxError("trailing input")
+        else:
+            self.raise_unexpected(tkn)
+
+    def match_expr(self):
+        tkn = self.lexer.next_token()
+        if tkn[0] == TOKEN_MEM:
+            self.assert_next(TOKEN_LBRACKET)
+            address = self.match_expr()
+            self.assert_next(TOKEN_RBRACKET)
+            return MemoryNode(address)
+        elif tkn[0] == TOKEN_INT:
+            try:
+                return IntNode(int(tkn[1], base=0))
+            except ValueError:
+                raise SyntaxError("invalid integer literal: {}".format(tkn[1]))
+        elif tkn[0] == TOKEN_REGISTER:
+            return RegisterNode(tkn[1])
+        else:
+            self.raise_unexpected(tkn)
+
+    def assert_next(self, typ):
+        tkn = self.lexer.next_token()
+        if tkn[0] != typ:
+            self.raise_unexpected(tkn)
+
+    def raise_unexpected(self, tkn):
+        if tkn[0] == TOKEN_EOF:
+            raise SyntaxError("premature end of input")
+        elif tkn[0] == TOKEN_UNKNOWN:
+            raise SyntaxError("unrecognized input `{}`".format(tkn[1]))
+        else:
+            raise SyntaxError("did not expect `{}` in this position".format(tkn[1]))
+
+
+MemoryNode = namedtuple("MemoryNode", ["address"])
+AssignNode = namedtuple("AssignNode", ["lhs", "rhs"])
+RegisterNode = namedtuple("RegisterNode", ["value"])
+IntNode = namedtuple("IntNode", ["value"])
+
+
+class MiniLexer:
+    """A lexer for the debugger's expression mini-language."""
+
+    def __init__(self, text):
+        self.text = text.lower()
+        self.position = 0
+
+    def next_token(self):
+        # Skip whitespace.
+        while self.position < len(self.text) and self.text[self.position].isspace():
+            self.position += 1
+
+        if self.position >= len(self.text):
+            return TOKEN_EOF, ""
+
+        ch = self.text[self.position]
+        if ch == "m":
+            return self.advance_and_return(TOKEN_MEM)
+        elif ch == "[":
+            return self.advance_and_return(TOKEN_LBRACKET)
+        elif ch == "]":
+            return self.advance_and_return(TOKEN_RBRACKET)
+        elif ch == "=":
+            return self.advance_and_return(TOKEN_ASSIGN)
+        elif ch in ("r", "p", "f", "s"):
+            length = self.read_register()
+            if length != -1:
+                return self.advance_and_return(TOKEN_REGISTER, length=length)
+            else:
+                return self.advance_and_return(TOKEN_UNKNOWN)
+        elif ch.isdigit():
+            length = self.read_int()
+            return self.advance_and_return(TOKEN_INT, length=length)
+        elif ch == "-":
+            self.position += 1
+            length = self.read_int()
+            self.position -= 1
+            return self.advance_and_return(TOKEN_INT, length=length)
+        else:
+            return self.advance_and_return(TOKEN_UNKNOWN)
+
+    def read_register(self):
+        ch = self.text[self.position]
+        if ch == "r":
+            if self.peek() == "t":
+                return 2
+            elif self.peek().isdigit():
+                length = 2
+                while self.peek(length).isdigit():
+                    length += 1
+                return length
+        elif ch == "p":
+            if self.text[self.position :].startswith("pc_ret"):
+                return 6
+            elif self.text[self.position :].startswith("pc"):
+                return 2
+        elif ch == "f":
+            if self.text[self.position :].startswith("fp_alt"):
+                return 6
+            elif self.text[self.position :].startswith("fp"):
+                return 2
+        elif ch == "s":
+            if self.peek() == "p":
+                return 2
+
+        # Default: not a register.
+        return -1
+
+    def read_int(self):
+        length = 1
+        digits = set([str(i) for i in range(10)])
+        if self.text[self.position] == "0" and self.peek() in ("b", "o", "x"):
+            length = 2
+            if self.peek() == "x":
+                digits |= set("abcdef")
+
+        while self.peek(length) in digits:
+            length += 1
+
+        return length
+
+    def peek(self, n=1):
+        return (
+            self.text[self.position + n] if self.position + n < len(self.text) else ""
+        )
+
+    def advance_and_return(self, typ, *, length=1):
+        start = self.position
+        self.position += length
+        return typ, self.text[start : start + length]
+
+
+TOKEN_INT = "TOKEN_INT"
+TOKEN_MEM = "TOKEN_MEM"
+TOKEN_REGISTER = "TOKEN_REGISTER"
+TOKEN_LBRACKET = "TOKEN_LBRACKET"
+TOKEN_RBRACKET = "TOKEN_RBRACKET"
+TOKEN_ASSIGN = "TOKEN_ASSIGN"
+TOKEN_EOF = "TOKEN_EOF"
+TOKEN_UNKNOWN = "TOKEN_UNKNOWN"
---
+++ test/test_minilanguage.py
@@ -0,0 +1,54 @@
+from hera.minilanguage import (
+    AssignNode,
+    IntNode,
+    MemoryNode,
+    MiniLexer,
+    MiniParser,
+    RegisterNode,
+    TOKEN_ASSIGN,
+    TOKEN_EOF,
+    TOKEN_INT,
+    TOKEN_LBRACKET,
+    TOKEN_MEM,
+    TOKEN_RBRACKET,
+    TOKEN_REGISTER,
+)
+
+
+def test_lex_mini_language_with_small_example():
+    lexer = MiniLexer("r1")
+
+    assert lexer.next_token() == (TOKEN_REGISTER, "r1")
+    assert lexer.next_token() == (TOKEN_EOF, "")
+    assert lexer.next_token() == (TOKEN_EOF, "")
+
+
+def test_lex_mini_language_with_big_example():
+    # This isn't a syntactically valid expression, but it doesn't matter to the lexer.
+    lexer = MiniLexer("M[FP_alt] = R15 0xabc")
+
+    assert lexer.next_token() == (TOKEN_MEM, "m")
+    assert lexer.next_token() == (TOKEN_LBRACKET, "[")
+    assert lexer.next_token() == (TOKEN_REGISTER, "fp_alt")
+    assert lexer.next_token() == (TOKEN_RBRACKET, "]")
+    assert lexer.next_token() == (TOKEN_ASSIGN, "=")
+    assert lexer.next_token() == (TOKEN_REGISTER, "r15")
+    assert lexer.next_token() == (TOKEN_INT, "0xabc")
+    assert lexer.next_token() == (TOKEN_EOF, "")
+    assert lexer.next_token() == (TOKEN_EOF, "")
+
+
+def test_parse_mini_language():
+    parser = MiniParser(MiniLexer("M[M[rt]] = 0xfab"))
+
+    tree = parser.parse()
+
+    assert isinstance(tree, AssignNode)
+
+    assert isinstance(tree.lhs, MemoryNode)
+    assert isinstance(tree.lhs.address, MemoryNode)
+    assert isinstance(tree.lhs.address.address, RegisterNode)
+    assert tree.lhs.address.address.value == "rt"
+
+    assert isinstance(tree.rhs, IntNode)
+    assert tree.rhs.value == 0xFAB
