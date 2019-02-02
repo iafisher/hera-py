cfde71ff3f02cb50c8e08187c233c09c287f52f8
Ian Fisher <iafisher@protonmail.com>
2019-02-02 14:46:40 -0500

    Attempt to fix failing build on Python 3.4

--- setup.py
+++ setup.py
@@ -3,35 +3,30 @@ from setuptools import find_packages, setup
 
 
 dpath = os.path.dirname(os.path.realpath(__file__))
-with open(os.path.join(dpath, 'README.md'), 'r') as f:
+with open(os.path.join(dpath, "README.md"), "r") as f:
     long_description = f.read()
 
 
 setup(
-    name='hera-py',
-    version='0.5.1',
+    name="hera-py",
+    version="0.5.1",
     description=(
-        'Interpreter for the Haverford Educational RISC Architecture (HERA) '
-        'assembly language'
+        "Interpreter for the Haverford Educational RISC Architecture (HERA) "
+        "assembly language"
     ),
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    license='MIT',
-    author='Ian Fisher',
-    author_email='iafisher@protonmail.com',
-    entry_points={
-        'console_scripts': [
-            'hera = hera.main:external_main',
-        ],
-    },
-    packages=find_packages(exclude=['tests']),
-    project_urls={
-        'Source': 'https://github.com/iafisher/hera-py',
-    },
+    long_description_content_type="text/markdown",
+    license="MIT",
+    author="Ian Fisher",
+    author_email="iafisher@protonmail.com",
+    entry_points={"console_scripts": ["hera = hera.main:external_main"]},
+    install_requires=["typing==3.6.6"],
+    packages=find_packages(exclude=["tests"]),
+    project_urls={"Source": "https://github.com/iafisher/hera-py"},
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Topic :: Software Development :: Assemblers',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Topic :: Software Development :: Assemblers",
     ],
 )
