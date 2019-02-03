ca597b8277e95e1f858472d88591614634786a18
Ian Fisher <iafisher@protonmail.com>
2019-02-02 19:41:33 -0500

    Set up profiling code

--- do_profile.py
+++ do_profile.py
@@ -6,5 +6,5 @@ from hera.main import external_main
 
 now = datetime.datetime.now()
 filename = "profile/profile-{0.year}-{0.month:0>2}-{0.day:0>2}-{0.hour:0>2}:{0.minute:0>2}".format(now)
-cProfile.run("external_main()")
+cProfile.run("external_main([\"profile/benchmark31.hera\"])", filename=filename)
 print("Wrote profile to {}".format(filename))
